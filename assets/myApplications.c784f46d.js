8dc29caba50247a4a8bdfe6f8e53eacf949e457b
lincongyi <88583354@163.com>
2023-06-07 15:44:43 +0800

    update

---
+++ assets/myApplications.c784f46d.js
@@ -0,0 +1,2 @@
+import{bm as p}from"./index.db61cdac.js";const e=t=>p.post("/user/getApplyCount",t),r=t=>p.post("/user/getApplyList",t),o=t=>p.post("/user/getApplyDetail",t),n=t=>p.post("/user/urging",t),u=t=>p.post("/user/stopApply",t);export{o as a,r as b,u as c,e as g,n as h};
+//# sourceMappingURL=myApplications.c784f46d.js.map
