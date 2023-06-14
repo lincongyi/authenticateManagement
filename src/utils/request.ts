99174baec38df19f6338054d058cc1181abf36dc
lincongyi <88583354@163.com>
2023-06-14 14:38:22 +0800

    refactor(custom): 优化
    1.axios cancelToken防止重复请求

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { message } from 'antd'
 import { loadEnv } from '@utils/index'
 const { MODE, VITE_AUTH_BASE_URL } = loadEnv()
@@ -7,11 +7,50 @@ const request = axios.create({
   timeout: 5000
 })
 
+/**
+ * 获取请求的key值
+ */
+const getRequestKey = (config: AxiosRequestConfig) => {
+  const { method, url } = config
+  return [method, url].join('')
+}
+
+const requestMap = new Map()
+
+/**
+ * 添加请求标记
+ */
+const addRequest = (config: AxiosRequestConfig) => {
+  const requestKey = getRequestKey(config)
+  if (requestMap.has(requestKey)) return
+
+  const CancelToken = axios.CancelToken
+  config.cancelToken =
+    config.cancelToken ||
+    new CancelToken(cancel => {
+      requestMap.set(requestKey, cancel)
+    })
+}
+
+/**
+ * 移除请求标记
+ */
+const removeRequest = (config: AxiosRequestConfig) => {
+  const requestKey = getRequestKey(config)
+  if (!requestMap.has(requestKey)) return
+
+  const cancel = requestMap.get(requestKey)
+  cancel()
+  requestMap.delete(requestKey)
+}
+
 // 添加请求拦截器
 request.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token')
     if (token) config.headers!.token = `${token}`
+    removeRequest(config)
+    addRequest(config)
     return config
   },
   error => {
@@ -22,6 +61,7 @@ request.interceptors.request.use(
 // 添加响应拦截器
 request.interceptors.response.use(
   response => {
+    removeRequest(response.config)
     const { retCode } = response.data
     if (!retCode) return response.data
     else {
@@ -39,6 +79,7 @@ request.interceptors.response.use(
     }
   },
   error => {
+    removeRequest(error.config)
     const { retMessage = '网络错误' } = error.response.data
     message.warning(retMessage)
     return Promise.reject(error)
