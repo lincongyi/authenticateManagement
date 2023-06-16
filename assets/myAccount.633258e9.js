c7985df4f9715f20b5afcd9154cee0223cc727b3
lincongyi <88583354@163.com>
2023-06-16 17:45:30 +0800

    update

---
+++ assets/myAccount.633258e9.js
@@ -0,0 +1,2 @@
+import{bk as n}from"./index.73fee95b.js";const e=()=>n.get("/account/currentCompanyInfo"),o=()=>n.get("/account/currentUserInfo"),a=t=>n.post("/account/updateAdminPhoneNum",t),s=t=>n.post("/account/updateAdminEmail",t),c=t=>n.post("/account/applyUpdateCompanyInfo",t),u=t=>n.post("/account/retrievePassword",t),p=t=>n.post("/account/currentResetPassword",t);export{p as a,e as b,o as c,a as d,c as e,u as r,s as u};
+//# sourceMappingURL=myAccount.633258e9.js.map
