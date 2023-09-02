da4252b0054dbf996cd4a4ec59eb4409f35f238a
congyi <88583354@163.com>
2023-09-02 21:28:15 +0800

    feat(custom): 微警2.0
    1.基础能力中心，暂无可接入此基础能力的应用

--- src/pages/AppServiceCenter/Ability/Introduction/index.tsx
+++ src/pages/AppServiceCenter/Ability/Introduction/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import style from './index.module.scss'
-import { Button, Divider, Tabs } from 'antd'
+import { Button, Divider, Tabs, Modal } from 'antd'
 import type { TabsProps } from 'antd'
 import BasicAbility from './components/BasicAbility'
 import Interface from './components/Interface'
@@ -22,15 +22,25 @@ const items: TabsProps['items'] = [
 const Introduction = () => {
   const [open, setOpen] = useState(false)
 
+  const [modal, contextHolder] = Modal.useModal()
+
   /**
    * 申请接入
    */
   const onAccess = () => {
-    setOpen(true)
+    Math.random() >= 0.5
+      ? setOpen(true)
+      : modal.warning({
+          title: '提示',
+          content:
+            '暂无可接入此基础能力的应用，去【我的应用】创建新应用后再添加此基础能力吧~',
+          okText: '确定'
+        })
   }
 
   return (
     <>
+      {contextHolder}
       <div className={style['sub-title']}>
         <p>
           身份认证能力简介说明，比如“多认证源，多认证链路可配置，支持高并发，安全匿名标识等”
