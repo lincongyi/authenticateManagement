29fb743954237eaaed45ea73c2c6a72158708908
congyi <88583354@163.com>
2023-09-03 16:37:30 +0800

    feat(custom): 微警2.0
    1.开发文档模块功能

--- src/pages/DevDocument/index.tsx
+++ src/pages/DevDocument/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import style from './index.module.scss'
-import { Row, Col, Tree } from 'antd'
+import { Row, Col, Tree, Divider, Select, Input } from 'antd'
 import type { DataNode, TreeProps } from 'antd/es/tree'
+import pdf from '@/assets/devDocument.pdf'
+
+const { Search } = Input
 
 const DevDocument = () => {
   const treeData: DataNode[] = [
@@ -45,32 +48,81 @@ const DevDocument = () => {
   ]
 
   const [selectedNode, setSelectedNode] = useState<string>()
+  /**
+   * 选择目录节点
+   */
   const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
     if (info.node.title) {
       const title = !info.node.children ? info.node.title : ''
       setSelectedNode(title as string)
     }
   }
+
+  /**
+   * 预览文档地址
+   */
+  // const [pdfUrl, setpdfUrl] = useState<string>()
+
+  const options = [
+    { value: '1', label: '身份认证' },
+    { value: '2', label: '人脸识别' },
+    { value: '3', label: '活体检测' }
+  ]
+  /**
+   *  切换项目
+   */
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`)
+  }
+
+  /**
+   * 搜索
+   */
+  const onSearch = (value: string) => {
+    console.log(`value ${value}`)
+  }
+
   return (
-    <Row className={style.container}>
-      <Col span={6}>
-        <div className={`${style.title} ${style.catalog} ${style.bdr}`}>
-          目录
-        </div>
-        <div className={`${style.content} ${style.bdr}`}>
-          <Tree
-            defaultExpandedKeys={['0-0', '0-0-3']}
-            defaultSelectedKeys={['0-0', '0-0-3']}
-            onSelect={onSelect}
-            treeData={treeData}
+    <>
+      <div className={style['document-header']}>
+        <div className={style['left-side']}>
+          项目：
+          <Select
+            defaultValue='2'
+            style={{ width: 200 }}
+            onChange={handleChange}
+            options={options}
           />
         </div>
-      </Col>
-      <Col span={18}>
-        <div className={style.title}>{selectedNode}</div>
-        <div className={style.content}></div>
-      </Col>
-    </Row>
+        <div className={style['right-side']}>
+          <Search
+            placeholder='请输入目录/文档关键词'
+            onSearch={onSearch}
+            enterButton
+          />
+        </div>
+      </div>
+      <Divider />
+      <Row className={style.container}>
+        <Col span={6}>
+          <div className={`${style.title} ${style['catalog-title']}`}>目录</div>
+          <div className={`${style.content} ${style['catalog-content']}`}>
+            <Tree
+              defaultExpandedKeys={['0-0', '0-0-3']}
+              defaultSelectedKeys={['0-0', '0-0-3']}
+              onSelect={onSelect}
+              treeData={treeData}
+            />
+          </div>
+        </Col>
+        <Col span={18}>
+          <div className={style.title}>{selectedNode}</div>
+          <div className={`${style.content} ${style['doc-content']}`}>
+            <iframe src={pdf} width='100%' height='100%'></iframe>
+          </div>
+        </Col>
+      </Row>
+    </>
   )
 }
 
