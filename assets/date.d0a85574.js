ba71a8691f443f419fa74d2c8d8c65aa4e5b43ad
lincongyi <88583354@163.com>
2023-10-10 18:25:38 +0800

    update

---
+++ assets/date.d0a85574.js
@@ -0,0 +1,1 @@
+import{d as a}from"./zh-cn.21cecca6.js";const s=[{label:"\u8FD17\u65E5",value:[a().add(-7,"d"),a()]},{label:"\u8FD114\u65E5",value:[a().add(-14,"d"),a()]},{label:"\u8FD130\u65E5",value:[a().add(-30,"d"),a()]}],l=d=>d&&d>=a().endOf("day"),t="YYYY-MM-DD";export{l as a,t as d,s as r};
