6159692a7dc1451096fb1bdb53957103e59683ef
lincongyi <88583354@163.com>
2023-10-11 19:22:06 +0800

    update

---
+++ assets/myAccount.5dbb90d4.js
@@ -0,0 +1,1 @@
+import{aK as s}from"./index.99f96971.js";const t=()=>s.get("/access/currentCompanyInfo"),n=()=>s.get("/access/currentUserInfo"),a=r=>s.post("/access/applyUpdateCompanyInfo",r),o=r=>s.post("/access/retrievePassword",r),c=r=>s.post("/access/currentResetPassword",r);export{c as a,t as b,n as c,a as d,o as r};
