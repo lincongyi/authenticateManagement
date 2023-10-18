31f1a63508bab37679a6fca3d2d8ba69a2d3c2ad
lincongyi <88583354@163.com>
2023-10-18 16:23:56 +0800

    update

---
+++ assets/ability.76dfe919.js
@@ -0,0 +1,1 @@
+import{aK as s}from"./index.f6c89b72.js";const e=t=>s.post("/access/getCapabilityList",t),p=t=>s.post("/access/getCapability",t),i=t=>s.post("/access/getAccessList",t),c=t=>s.post("/access/addAppCapabilityForm",t),r=t=>s.post("/access/getAppCapabilityForm",t);export{p as a,i as b,r as c,c as d,e as g};
