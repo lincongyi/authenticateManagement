d8967b71e78d6f4dbfe840262a9086d9911dc2a8
lincongyi <88583354@163.com>
2023-10-31 17:08:14 +0800

    feat(custom): 微警2.0
    1.消息中心，成功注册模板

--- src/pages/MessageCenter/MessageDetail/index.tsx
+++ src/pages/MessageCenter/MessageDetail/index.tsx
@@ -1,54 +1,181 @@
 import React, { useEffect, useState } from 'react'
-import { getMsgDetail } from '@api/messageCenter'
+import { TGetMsgDetail, getMsgDetail } from '@api/messageCenter'
 import { useSearchParams } from 'react-router-dom'
 import AccessRegisterTemplate from './components/AccessRegisterTemplate'
+import { Col, Row } from 'antd'
 
 const MessageDetail = () => {
   const [searchParams] = useSearchParams()
   const id = Number(searchParams.get('id'))
 
-  const [content, setContent] = useState<Record<string, string>>() // 消息主要内容
-
-  const [type, setType] = useState<TApplyKey>()
+  const [info, setInfo] = useState<TGetMsgDetail>() // 消息主要内容
 
   useEffect(() => {
     ;(async () => {
       const { data } = await getMsgDetail({ id })
       if (!data) return
-      console.log(data)
-      setContent(JSON.parse(data.content))
-      console.log(data.type)
-      setType(data.type)
+      setInfo(data)
     })()
   }, [])
   return (
     <>
-      {(() => {
-        switch (type) {
-          case 'ACCESS_REGISTER':
-            return <AccessRegisterTemplate />
-          case 'UPDATE_COMPANY_INFO':
-            return <AccessRegisterTemplate />
-          case 'ACCESS_CAPABILITY':
-            return <AccessRegisterTemplate />
-          case 'PROD_CAPABILITY':
-            return <AccessRegisterTemplate />
-          case 'APPLY_FILE_CAPABILITY':
-            return <AccessRegisterTemplate />
-          case 'STOP_APPLICATION':
-            return <AccessRegisterTemplate />
-          case 'START_APPLICATION':
-            return <AccessRegisterTemplate />
-          case 'CAPABILITY_UPDATE':
-            return <AccessRegisterTemplate />
-          case 'ADD_NUM_APPLICATION':
-            return <AccessRegisterTemplate />
-          case 'DELAY_CAPABILITY':
-            return <AccessRegisterTemplate />
-          default:
-            return <></>
-        }
-      })()}
+      {info &&
+        (() => {
+          switch (info.type) {
+            case 'ACCESS_REGISTER':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'UPDATE_COMPANY_INFO':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'ACCESS_CAPABILITY':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'PROD_CAPABILITY':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'APPLY_FILE_CAPABILITY':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'STOP_APPLICATION':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'START_APPLICATION':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'CAPABILITY_UPDATE':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'ADD_NUM_APPLICATION':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            case 'DELAY_CAPABILITY':
+              return (
+                <>
+                  {info.content && (
+                    <Row style={{ marginTop: 20 }}>
+                      <Col
+                        xs={{ span: 20, offset: 2 }}
+                        xxl={{ span: 16, offset: 4 }}
+                      >
+                        <AccessRegisterTemplate info={info} />
+                      </Col>
+                    </Row>
+                  )}
+                </>
+              )
+            default:
+              return <></>
+          }
+        })()}
     </>
   )
 }
