cb390da37439c5cc7b070ec251373c3a442d094a
lincongyi <88583354@163.com>
2023-11-02 17:38:04 +0800

    update

---
+++ assets/ability.563f5721.js
@@ -0,0 +1,1 @@
+import{aH as s}from"./index.c52fca20.js";const p=t=>s.post("/access/getCapabilityList",t),e=t=>s.post("/access/getCapability",t),i=t=>s.post("/access/getAccessList",t),c=t=>s.post("/access/addAppCapabilityForm",t),r=t=>s.post("/access/getAppCapabilityForm",t),o=t=>s.post("/access/editAppCapabilityForm",t);export{e as a,i as b,r as c,c as d,o as e,p as g};
