8e3b2eeb381e297a7fa3e2f020a59c1c468ea305
lincongyi <88583354@163.com>
2023-11-08 15:50:21 +0800

    update

---
+++ assets/useBreakpoint.a11f41e8.js
@@ -0,0 +1,1 @@
+import{r as o,b1 as a,aO as c,$ as f}from"./index.65cdaf36.js";function p(){let s=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!0;const e=o.exports.useRef({}),t=a(),r=c();return f(()=>{const n=r.subscribe(u=>{e.current=u,s&&t()});return()=>r.unsubscribe(n)},[]),e.current}export{p as u};
