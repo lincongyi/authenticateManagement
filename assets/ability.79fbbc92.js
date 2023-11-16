808554c33355c922ba59459cc24e8d04d5e57818
lincongyi <88583354@163.com>
2023-11-16 20:42:30 +0800

    update

---
+++ assets/ability.79fbbc92.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index.31ab55ba.js";const p=t=>s.post("/access/getCapabilityList",t),e=t=>s.post("/access/getCapability",t),i=t=>s.post("/access/getAccessList",t),c=t=>s.post("/access/addAppCapabilityForm",t),r=t=>s.post("/access/getAppCapabilityForm",t),o=t=>s.post("/access/editAppCapabilityForm",t);export{i as a,p as b,r as c,c as d,o as e,e as g};
