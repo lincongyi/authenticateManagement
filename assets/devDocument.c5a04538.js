2d779cf4e52ff381adaf6d390bc935ca93ead615
lincongyi <88583354@163.com>
2023-11-28 17:27:39 +0800

    update

---
+++ assets/devDocument.c5a04538.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index.69846dde.js";const c=()=>t.post("/access/getDirectory",{projectComponent:"DEV_FILE_LIST"}),s=e=>t.post("/access/queryDocument",e),o=e=>t.post("/access/devfileSearch",e),a=e=>t.post("/access/getProjectId",e);export{a,o as d,c as g,s as q};
